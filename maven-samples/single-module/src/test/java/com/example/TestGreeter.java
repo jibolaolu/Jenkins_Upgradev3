60592d6b5b4bc63dd63da48548a91903d75220f7
jibolaolu <seun_2006@yahoo.com>
2020-09-24 13:38:45 +0100

    change of method

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessages() {
     String someone = "Hello World, The name of my daughter is OluwaTamilore";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
