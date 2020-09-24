c86285c3caf3ac9803783c6a418f2416c3e2be0c
jibolaolu <seun_2006@yahoo.com>
2020-09-24 13:35:50 +0100

    Changes to greeter

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Hello World, The name of my daughter is OluwaTamilore";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
